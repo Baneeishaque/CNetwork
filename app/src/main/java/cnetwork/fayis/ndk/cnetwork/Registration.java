d0988fbae6ee8fe851e2e237cb524558a3d1f95a
Banee Ishaque K <baneeishaque@gmail.com>
2017-10-21 19:34:08 +0530

    first commit

---
+++ app/src/main/java/cnetwork/fayis/ndk/cnetwork/Registration.java
@@ -0,0 +1,28 @@
+package cnetwork.fayis.ndk.cnetwork;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class Registration extends AppCompatActivity {
+
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+    }
+
+
+}
